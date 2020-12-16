9f421fc607b031e0c3096349cf04ba2a1f22354e
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro.local>
2020-12-15 23:41:21 -0500

    completed 4 of 10 eventlistners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+
+const busPicture = document.querySelector('img');
+console.group(busPicture);
+
+busPicture.addEventListener('mouseover', ()=> {
+    alert("over")
+})
+
+ 
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', (e) => {
+        console.log( `${e.code}`)
+})
+
+
+
+ window.addEventListener('load', function() {
+    console.log("window has loaded successfully")
+ });
+
+
+  const btns = document.querySelector('.btn');
+
+  btns.addEventListener('dblclick', function() {
+            btns.textContent = "clicked"
+            btns.style.color = "orange";
+            btns.style.backgroundColor = "blue"; 
+  })
+
+//   const p = document.querySelector('p');
+
+//   console.log(p)
+//   p.addEventListener('select', function() {
+//       p.style.color = "gray";
+//       p.textContent = "I was changed"
+//   })
